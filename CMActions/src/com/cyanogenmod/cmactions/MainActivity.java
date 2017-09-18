d7901596c3dc8f30d4d166caf78a36e340d583ad
gabrielgagz <g.a.gomez@gmail.com>
2017-09-18 01:39:15 -0300

    CMActions: Revert to PreferenceActivity Also, start using the stock theme from settings, and still needs some adjustment related to initial state...

--- CMActions/src/com/cyanogenmod/cmactions/MainActivity.java
+++ CMActions/src/com/cyanogenmod/cmactions/MainActivity.java
@@ -16,102 +16,26 @@
 
 package com.cyanogenmod.cmactions;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Switch;
-import android.os.SystemProperties;
-import android.util.Log;
-import java.io.*;
-import java.util.prefs.*;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity {
 
-private Switch DeviceModeSwitch;
-
-	// Write into usb_device file
-    private void handleDeviceMode(String value) {
-    try {
-			String f = "/proc/usb_device";
-			FileWriter w = new FileWriter(f);
-			BufferedWriter bw = new BufferedWriter(w);
-			PrintWriter wr = new PrintWriter(bw);
-			wr.write(value); 
-			wr.close();
-			bw.close();
-            } catch(IOException e) {
-            e.printStackTrace();
-			}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new DeviceModeFragment()).commit();
     }
-    
-    // Get the system property
-    public static String getsysprop() {
-    InputStreamReader in = null;
-    BufferedReader reader = null;
-    try {
-        Process proc = Runtime.getRuntime().exec(new String[]{"/system/bin/getprop", "persist.sys.rzr.device_mode"});
-        in = new InputStreamReader(proc.getInputStream());
-        reader = new BufferedReader(in);
-        return reader.readLine();
-    } catch (IOException e) {
-        return null;
-    } finally {
-        closeQuietly(in);
-        closeQuietly(reader);
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
     }
-}
-
-public static void closeQuietly(Closeable closeable) {
-    if (closeable == null) return;
-    try {
-        closeable.close();
-    } catch (IOException ignored) {
-    }
-}
-	
-	// Set the system property
-	private void setsysprop(String value) {
-	try {
-	String[] cmd = { "/system/bin/sh", "-c", "setprop persist.sys.rzr.device_mode " + value};	
-    Runtime.getRuntime().exec(cmd);
-	} catch (java.io.IOException e) {
-	}
-	}
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-
-DeviceModeSwitch = (Switch) findViewById(R.id.DeviceModeSwitch);
-
-// Initial state
-String initialstate = getsysprop();
-if (initialstate.equals("true")) { DeviceModeSwitch.setChecked(true); } else { DeviceModeSwitch.setChecked(false); }
-
-// attach a listener to check for changes in state
-DeviceModeSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-@Override
-public void onCheckedChanged(CompoundButton buttonView,
-	boolean isChecked) {
-
-		if (isChecked) {
-
-			handleDeviceMode("0");
-			setsysprop("true");
-			Log.i("Device Mode","On");
-
-			} else {
-
-			handleDeviceMode("1");
-			setsysprop("false");
-			Log.i("Device Mode","Off");
-		}
-
-		}
-		});
-}
 }
